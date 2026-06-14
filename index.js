2bdd0b880aa4fe0e609fc45b679db87654148c9f
khalidelhimer7777-beep <khalidelhimer7777@gmail.com>
2026-06-14 14:49:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '+212767282589', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
